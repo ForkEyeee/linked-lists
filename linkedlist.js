e943330e514d3c479da47b60e47395c1ddd9a17c
ForkEyeee <shawncarter123456@gmail.com>
2023-05-31 03:01:02 -0400

    added linkedlist and node classes

--- linkedlist.js
+++ linkedlist.js
@@ -1,7 +1,21 @@
 class LinkedList {
-	
+  static list = [2]
+
+  static nodeList() {
+    return this.list
+  }
 }
 
 class Node {
-	
+	constructor(value, nextNode) {
+		this.value = value
+		this.nextNode = nextNode
+	}
+
+	static append(value) {
+
+	}
+
 }
+
+console.log(LinkedList.nodeList())
